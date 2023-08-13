8af63ddbd85eb3f431ab1f4768476c0b2515b84f
Chevyjd2007 <112202840+Chevyjd2007@users.noreply.github.com>
2023-08-13 14:56:32 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Home from './components/home/Home'
+
+function App() {
+  
+
+  return (
+   <main className="main">
+    <Home/>
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import ProfileImg from "../../assets/profile-Img.png"
+import shapeOne from "../../assets/shape-1.png"
+import shapeTwo from "../../assets/shape-2.png"
+
+import { RxGithubLogo, RxLinkedinLogo } from "react-icons/rx";
+import { BsDiscord } from "react-icons/bs";
+import  "./home.css";
+
+const Home = () => {
+  return <section className='home' id='home'>
+    <div className="home__container container">
+        <p className="home__subtitle text-cs">
+            What's up, <span>My name Is</span>
+        </p>
+
+        <h1 className="home__title text-cs"><span>Josias</span> Chevalier</h1>
+
+        <p className="home__job">
+            <span className="text-cs">I have experience with</span> <b>Full Stack Development</b>
+        </p>
+
+        <div className="home__img-wrapper">
+            <div className="home__banner">
+                <img src={ProfileImg} alt="" className='home__profile' />
+            </div>
+
+            <img src="" alt={shapeOne} className="shape shape__1" />
+            <img src="" alt={shapeTwo} className="shape shape__2" />
+            <img src="" alt={shapeTwo} className="shape shape__3" />
+        </div>
+
+        <p className="home__text">
+            From Miami, Florida. I have a Bachelor of Arts in Computer Science from Florida International University.
+        </p>
+
+        <div className="home__socials">
+            <a href="https://github.com/Chevyjd2007" className="home__social-link">
+                <RxGithubLogo/>
+            </a>
+            <a href="https://www.linkedin.com/in/josias-chevalier-10882964/" className="home__social-link">
+                <RxLinkedinLogo/>
+            </a>
+            <a href="https://discordapp.com/users/che.vy" className="home__social-link">
+                <BsDiscord/>
+            </a>
+        </div>
+
+        <div className="home__btns">
+            <a href="" className="btn text-cs">Download CV</a>
+
+            <a href="" className="hero__link text-cs">My Skills</a>
+        </div>
+    </div>
+  </section>;
+    
+  
+}
+
+export default Home
